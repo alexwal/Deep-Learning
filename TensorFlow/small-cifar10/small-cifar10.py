862f6ff3155e4a9136173e3fe369471a1a87f025
Alex Walczak <awal@berkeley.edu>
2017-05-29 15:16:04 -0700

    simple training works! (lenet model) todo: replace final layer with logsoftmax

--- TensorFlow/small-cifar10/small-cifar10.py
+++ TensorFlow/small-cifar10/small-cifar10.py
@@ -6,8 +6,11 @@ import tensorflow as tf
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import math
 import os
 from six.moves import xrange # pylint: disable=redefined-builtin
+
+sqrt = math.sqrt
 
 '''
   # Train TF model on small cifar10 dataset.
@@ -52,7 +55,7 @@ class BatchProvider(object):
     self.idx += batch_size
     if len(batch) < batch_size: # self.idx is past end of data.
       # maybe re-shuffle data now?
-      print('Reached end of data. Extending this batch by wrapping around to front.')
+      # print('Reached end of data. Extending this batch by wrapping around to front.')
       self.idx = self.idx % len(self.data)
       additional_data = self.data[:self.idx]
       additional_labels = self.labels[:self.idx]
@@ -60,14 +63,17 @@ class BatchProvider(object):
       batch_labels = np.concatenate((batch_labels, additional_labels))
     return batch, batch_labels
 
+  def __len__(self):
+    return len(self.data)
+
 class Read_data_sets(object):
   def __init__(self, data_dir, train_data_file, train_labels_file,
                 test_data_file, test_labels_file):
     # Load data files
     train_data = np.load(os.path.join(data_dir, train_data_file)) 
-    train_labels = np.load(os.path.join(data_dir, train_labels_file)) 
+    train_labels = np.load(os.path.join(data_dir, train_labels_file)) - 1 #TODO: get rid of -1 <<-- --vv
     test_data = np.load(os.path.join(data_dir, test_data_file)) 
-    test_labels = np.load(os.path.join(data_dir, test_labels_file)) 
+    test_labels = np.load(os.path.join(data_dir, test_labels_file)) - 1
     # Create batch providers
     self.train = BatchProvider(train_data, train_labels) # usage ex: Read_data_sets.train.next_batch(batch_size)
     self.test = BatchProvider(test_data, test_labels)
@@ -80,17 +86,17 @@ data_sets = Read_data_sets('small-cifar10-data/',
 classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 batch_size = 16
 default_dtype = tf.float32
-default_learning_rate = 1e-2
+default_learning_rate = 1e-3
 im_H, im_W = 32, 32
 
 # UTILS:
 
-def make_variable(name, shape, stddev=None, constant=None):
+def make_var(name, shape, stddev=None, constant=None):
   # On CPU. Later: add weight regularization
-  assert stddev is not None and constant is not None, 'Stddev and constant cannot both be None: one determines the initializer used.'
+  assert (stddev is None) != (constant is None), 'Stddev and constant cannot both be None: one determines the initializer used.'
   with tf.device('/cpu:0'):
     if stddev is not None:
-      initializer = tf.truncated_normal(stddev=stddev, dtype=default_dtype) # values stay within two stddevs of mean (default mean = 0)
+      initializer = tf.truncated_normal_initializer(stddev=stddev, dtype=default_dtype) # values stay w/in 2 stddev of mean (default mean = 0)
     elif constant is not None:
       initializer = tf.constant_initializer(constant)
     var = tf.get_variable(name, shape, initializer=initializer, dtype=default_dtype)
@@ -115,57 +121,74 @@ def inputs(data_set):
 def inference(images):
   '''Compute inference on the model inputs to make a prediction.'''
 
-  # conv1 [32 x 32 x 3 -> 32 x 32 x 64]
+  '''
+    model:add(nn.SpatialConvolution(3, 6, 5, 5)) 
+    model:add(nn.ReLU())
+    model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+    model:add(nn.SpatialConvolution(6, 16, 5, 5))
+    model:add(nn.ReLU())
+    model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+    model:add(nn.View(16*5*5))
+    model:add(nn.Linear(16*5*5, 120))
+    model:add(nn.ReLU())
+    model:add(nn.Linear(120, 84))
+    model:add(nn.ReLU())
+    model:add(nn.Linear(84, 10))
+    model:add(nn.LogSoftMax())
+  '''
+
+  # conv1 [32 x 32 x 3 -> ] 
   with tf.variable_scope('conv1') as scope:
-    kernel = make_var('weights', [5, 5, 3, 64], stddev=0.005)
-    conv = tf.nn.conv2d(images, kernel, strides=[1, 1, 1, 1], padding='SAME')
-    biases = make_var('biases', [64], constant=0.0)
+    kernel = make_var('weights', [5, 5, 3, 6], stddev=sqrt(1.0 / (32 * 32 * 3))) # Initialize weights by drawing from distribution w/ stddev=1/sqrt(fan-in)
+    conv = tf.nn.conv2d(images, kernel, strides=[1, 1, 1, 1], padding='VALID') # For VALID conv: output dims: int((inW - kW)/stride + 1)
+    biases = make_var('biases', [6], constant=0.05)
     pre_activation = tf.nn.bias_add(conv, biases)
     # Spatial Batch Norm here?
     conv1 = tf.nn.relu(pre_activation, name=scope.name)
 
-  # pool1 (2x2) [32 x 32 x 64 -> 16 x 16 x 64]
+  # pool1 (2x2) 
   pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                         padding='SAME', name='pool1')
+                         padding='VALID', name='pool1')
 
-  # conv2 [16 x 16 x 64 -> 16 x 16 x 48]
+  # conv2 
   with tf.variable_scope('conv2') as scope:
-    kernel = make_var('weights', [3, 3, 64, 48], stddev=0.005)
-    conv = tf.nn.conv2d(pool1, kernel, strides=[1, 1, 1, 1], padding='SAME')
-    biases = make_var('biases', [48], constant=0.0)
+    kernel = make_var('weights', [5, 5, 6, 16], stddev=sqrt(1.0 / (14 * 14 * 6)))
+    conv = tf.nn.conv2d(pool1, kernel, strides=[1, 1, 1, 1], padding='VALID')
+    biases = make_var('biases', [16], constant=0.1)
     pre_activation = tf.nn.bias_add(conv, biases)
     # Spatial Batch Norm here?
     conv2 = tf.nn.relu(pre_activation, name=scope.name)
 
-  # conv3 [16 x 16 x 48 -> 16 x 16 x 24]
-  with tf.variable_scope('conv3') as scope:
-    kernel = make_var('weights', [3, 3, 48, 24], stddev=0.005)
-    conv = tf.nn.conv2d(conv2, kernel, strides=[1, 1, 1, 1], padding='SAME')
-    biases = make_var('biases', [48], constant=0.0)
-    pre_activation = tf.nn.bias_add(conv, biases)
-    # Spatial Batch Norm here?
-    conv3 = tf.nn.relu(pre_activation, name=scope.name)
 
-  # pool2 [16 x 16 x 24 -> 8 x 8 x 24]
-  pool2 = tf.nn.max_pool(conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                         padding='SAME', name='pool2')
+  # pool2 
+  pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                         padding='VALID', name='pool2')
 
-  # fc1 [8 x 8 x 24 -> 8*8*64 -> 128]
-  flattened = tf.reshape(pool2, [-1, 8 * 8 * 24])
+  # fc1 
   with tf.variable_scope('fc1') as scope:
+    flattened = tf.reshape(pool2, [-1, 5 * 5 * 16])
     # Dropout/keep_prob?
-    W = make_var('weights', [8 * 8 * 24, 128], stddev=0.001) 
-    linear = tf.nn.matmul(pool2, W)
-    biases = make_var('biases', [128], const=0.0)
+    W = make_var('weights', [5 * 5 * 16, 120], stddev=sqrt(1.0 / (5 * 5 * 16)))
+    linear = tf.matmul(flattened, W)
+    biases = make_var('biases', [120], constant=0.1)
     pre_activation = tf.add(linear, biases)
     fc1 = tf.nn.relu(pre_activation, name=scope.name)
 
-  # logits [128 -> num_classes=10]
+  # fc2 
+  with tf.variable_scope('fc2') as scope:
+    # Dropout/keep_prob?
+    W = make_var('weights', [120, 84], stddev=sqrt(1.0 / 120)) 
+    linear = tf.matmul(fc1, W)
+    biases = make_var('biases', [84], constant=0.1)
+    pre_activation = tf.add(linear, biases)
+    fc2 = tf.nn.relu(pre_activation, name=scope.name)
+
+  # logits [-> num_classes=10]
   with tf.variable_scope('logits') as scope:
     num_classes = len(classes)
-    W = make_var('weights', [128, num_classes], stddev=0.004)
-    linear = tf.matmul(fc1, W)
-    biases = make_var('biases', [num_classes], constant=0.0)
+    W = make_var('weights', [84, num_classes], stddev=sqrt(1.0 / 84))
+    linear = tf.matmul(fc2, W)
+    biases = make_var('biases', [num_classes], constant=0.1)
     pre_activation = tf.add(linear, biases)
     logits = tf.nn.relu(pre_activation, name=scope.name)
 
@@ -173,8 +196,8 @@ def inference(images):
 
 def loss(logits, labels):
   labels = tf.cast(labels, tf.int32)
-  cross_entropy = tf.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name='xent')
-  return tf.reduce_mean(cross_entropy, 'mean_xent')
+  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name='xent')
+  return tf.reduce_mean(cross_entropy, name='mean_xent')
 
 def training(total_loss, learning_rate):
   # Note: this isn't called over and over again when training; it's called
@@ -193,9 +216,11 @@ def evaluation(logits, labels):
 
 ###
 
-def do_eval(sess, eval_correct, images_pl, labels_pl, data_set):
+def do_eval(sess, eval_correct, images_pl, labels_pl, data_set, max_batches=None):
   '''Evaluation over one epoch (all samples) in data_set.'''
   num_batches = len(data_set) // batch_size
+  if max_batches is not None:
+    num_batches = min(num_batches, max_batches)
   num_samples = num_batches * batch_size
   correct_count = 0
   for batch in xrange(num_batches):
@@ -212,23 +237,29 @@ def run_training():
   train_op = training(total_loss, default_learning_rate)
   eval_correct = evaluation(logits, labels_pl)
   
+  # Setup session
+  sess = tf.Session()
   init = tf.global_variables_initializer()
   sess.run(init)
 
-  for step in xrange(1000):
-    feed_dict = fill_feed_dict(data_sets.train, images_pl, labels_pl)
-    _, loss_value = sess.run([train_op, total_loss], feed_dict=feed_dict) # discard train_op run output
+  # Perform training and evaluation
+  for step in xrange(4000):
+    feed_dict = fill_feed_dict(data_sets.train, images_pl, labels_pl) # each step trains on a single batch
+    _, train_loss_value = sess.run([train_op, total_loss], feed_dict=feed_dict) # discard train_op run output
 
-    if step % 100 == 0:
-      print("Step %d, step loss_value: %0.04f" % (step, loss_value))
-      print("Evaluating training data:")
-      do_eval(sess, eval_correct, images_pl, labels_pl, data_set.train)
-      print("Evaluating testing data:")
-      do_eval(sess, eval_correct, images_pl, labels_pl, data_set.test)
+    if step % 300 == 0:
+      print("\nStep %d, step train_loss_value: %0.06f" % (step, train_loss_value))
+      print("==> Evaluating training data:")
+      do_eval(sess, eval_correct, images_pl, labels_pl, data_sets.train, max_batches=10)
+      print("==> Evaluating testing data:")
+      do_eval(sess, eval_correct, images_pl, labels_pl, data_sets.test, max_batches=10)
+
+  print("\nFinal results:")
+  print("==> Evaluating training data:")
+  do_eval(sess, eval_correct, images_pl, labels_pl, data_sets.train)
+  print("==> Evaluating testing data:")
+  do_eval(sess, eval_correct, images_pl, labels_pl, data_sets.test)
 
 if __name__ == '__main__':
   run_training()
-
-
-
 
